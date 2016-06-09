079ff00c7914b2fe633bbdbdbd64aa137e0a8bc3
Shade10 <marcin.kass10@gmail.com>
2016-06-10 01:30:00 +0200

    add new classes responsible for creating new rooms and assigning guests to their room and first modification MainWindow GUI

---
+++ BookingRoomApp/Guest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingRoomApp
+{
+    public class Guest: Room
+    {
+        
+        
+        public string UserName { get; set; } = "";
+        public string UserSurname { get; set; } = "";
+        public DateTime EntryTimeRoom { get; set; } 
+        public DateTime QuieTimeRoom { get; set; }
+
+
+        public Guest() { }
+        public Guest(int nrRoom, string userName, string userSurname, DateTime entryTimeRoom, DateTime quieTimeRoom)
+        {
+            if (StatusRoom == Status.Free)
+            {
+                this.NrRoom = nrRoom;
+                this.UserName = userName;
+                this.UserSurname = userSurname;
+                this.EntryTimeRoom = entryTimeRoom;
+                this.QuieTimeRoom = quieTimeRoom;
+                this.StatusRoom = Status.Busy;
+            }
+        }
+    }
+}
---
+++ BookingRoomApp/Room.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingRoomApp
+{
+    public enum Status { Free, Busy, InReconstruction }
+    public enum HowManyBedInRooms { Single, Double, Triple }
+
+    
+
+    public class Room
+    {
+        public int NrRoom { get; set; }
+        public Status StatusRoom { get; set; } = Status.Free;
+        public HowManyBedInRooms HowManyBedInRoom { get; set; } = HowManyBedInRooms.Single;
+
+        public Room() { }
+
+        public Room(int nrRoom, Status statusRoom, HowManyBedInRooms howManyBedInRoom)
+        {
+            this.NrRoom = nrRoom;
+            this.StatusRoom = statusRoom;
+            this.HowManyBedInRoom = howManyBedInRoom;
+        }
+
+        
+
+    }
+}
